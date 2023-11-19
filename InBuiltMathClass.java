c2b562e018607e3e2e64a5f67928c60cd8bbd3dd
vijayguptagh <120735063+vijayguptagh@users.noreply.github.com>
2023-11-19 12:50:27 +0530

    Add files via upload

---
+++ InBuiltMathClass.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+class InBuiltMathClass{
+    public static void main(String args[]){
+         System.out.println("square root of 26 = "+Math.sqrt(26));
+         //int(Math.method()) gives error 
+         System.out.println("2 raise to power 3  = "+Math.pow(2,3));
+         System.out.println("log of 2.71 = "+Math.log(2.71));
+         System.out.println("log 0f 100 with base 10 = "+Math.log10(100));
+         System.out.println("e raise 1 = "+Math.exp(1));
+         System.out.println("sin value of angle pie/2(90degree) = "+Math.sin(3.14/2));
+         System.out.println("absolute value of -1.234 = "+Math.abs(-1.234));
+         System.out.println("maximum of 1,24 = "+Math.max(24,1));
+         System.out.println("rounded value of 7.123 = "+Math.round(7.123));
+
+    }
+}
+
+
