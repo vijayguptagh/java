86338534e9069a8722a1455823cc026b72cc6e9e
vijayguptagh <120735063+vijayguptagh@users.noreply.github.com>
2023-11-19 12:49:31 +0530

    Add files via upload

---
+++ FinalKeyword.java
@@ -0,0 +1,13 @@
+class A{
+void change(){
+            int a=20;
+            System.out.println("a = "+a);
+        }
+}
+public class FinalKeyword {
+    public static void main(String[] args) {
+                final int v=10;
+        System.out.println("v = "+v);
+
+    }
+}
