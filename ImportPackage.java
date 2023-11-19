8f5d8f0d2ef8a0e082945e84fea10ec3612eebe6
vijayguptagh <120735063+vijayguptagh@users.noreply.github.com>
2023-11-19 12:50:01 +0530

    Add files via upload

---
+++ ImportPackage.java
@@ -0,0 +1,11 @@
+//import package as package.class and call method as class.method
+import pack.A;
+class ImportPackage{
+    public static void main(String[] args) {
+        A o1=new A();
+
+        o1.a();
+
+        System.out.println("Package imported successfully");
+    }
+}
