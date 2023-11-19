c15c93a8ba5526a5bff224f189ad0149a9c2d1f1
vijayguptagh <120735063+vijayguptagh@users.noreply.github.com>
2023-11-19 12:52:47 +0530

    Add files via upload

---
+++ TypeConversionExample.java
@@ -0,0 +1,30 @@
+//type conversion
+
+class TypeConversionExample{
+    public static void main(String args[]){
+        //to clr screen
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+                       
+        //widening conversion allowed-implicit conversion
+        int a=2;
+        float b=a;
+        System.out.println("b = "+b);
+        
+        //narowwing conversion not allowed
+        float c=3.14f;
+        /*int d=c;
+        System.out.println("d = "+d);*/
+        
+        //typecasting -explicit conversion
+        System.out.println("b = "+(float)a);
+        System.out.println("d = "+(int)c);
+
+
+    }
+}   
+        
+        
+        
+        
+        
